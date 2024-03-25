e5596591e0383016ef9f43512dbbdf9d0784b366
unknown <patildevang04@gmail.com>
2024-03-25 23:31:03 +0530

    first commit

---
+++ src/main/java/com/crio/codingame/commands/CommandInvoker.java
@@ -0,0 +1,29 @@
+package com.crio.codingame.commands;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.crio.codingame.exceptions.NoSuchCommandException;
+
+public class CommandInvoker {
+    private static final Map<String, ICommand> commandMap = new HashMap<>();
+
+    public void register(String commandName, ICommand command){
+        commandMap.put(commandName,command);
+    }
+
+    private ICommand get(String commandName){
+        return commandMap.get(commandName);
+    }
+
+    public void executeCommand(String commandName, List<String> tokens) throws NoSuchCommandException {
+        ICommand command = get(commandName);
+        if(command == null){
+            // Handle Exception
+            throw new NoSuchCommandException();
+        }
+        command.execute(tokens);
+    }
+
+}
